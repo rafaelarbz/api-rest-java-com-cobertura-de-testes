17783cc89270795c909661c171f4b9fcbbee98eb
rafaela.souza <rafaela.souza@techlead.com.br>
2022-11-14 13:16:54 -0300

    User Class

--- api/src/main/java/br/com/rafaelarbz/api/api/ApiApplication.java
+++ api/src/main/java/br/com/rafaelarbz/api/api/ApiApplication.java
@@ -3,7 +3,6 @@ package br.com.rafaelarbz.api.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.rafaelarbz.api.api.domain.User;
 
 @SpringBootApplication
 public class ApiApplication {
